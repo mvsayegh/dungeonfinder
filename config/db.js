5c0d790f5b0bb21aa84aed1b091291afaeab01aa
Marcus Vinicius <marcus.sayegh@joit.com.br>
2025-04-14 18:45:51 -0300

    Adição do .gitignore.

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    console.log('✅ MongoDB connected successfully! ');
+    console.log('✅ Everything is working fine, you can start using the application now!');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const connectDB = require('./config/db');
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server running on port [${PORT}]`));
 
